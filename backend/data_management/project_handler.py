7dd024f828a8f6e0e3a4e67106410f4f92b89c35
Moritz Olthoff <moritz.olthoff@t-online.de>
2025-12-20 17:19:19 +0100

    Add project methods to tests

--- backend/data_management/project_handler.py
+++ backend/data_management/project_handler.py
@@ -1,5 +1,9 @@
+import json
+
 from asyncpg import Connection
 import uuid
+
+from pydantic import Json
 
 
 async def create_project(user_connection:Connection, data_connection:Connection, project_name: str, owner_id: int):
@@ -7,7 +11,7 @@ async def create_project(user_connection:Connection, data_connection:Connection,
 
     await data_connection.execute(f'CREATE SCHEMA "{project_id}"')
     await user_connection.execute('INSERT INTO projects (project_id, project_name, owner_id) VALUES ($1, $2, $3)', project_id, project_name, owner_id)
-    await user_connection.execute('INSERT INTO project_members (project_id, user_id, role) VALUES ($1, $2, $3)', project_id, owner_id, 'owner')
+    await user_connection.execute('INSERT INTO project_members (project_id, user_id, permission) VALUES ($1, $2, $3::json)', project_id, owner_id, json.dumps({"temp": "owner"}))
 
     return str(project_id)
 
@@ -30,19 +34,19 @@ async def project_exists(user_connection:Connection, project_id: str) -> bool:
     result = await user_connection.fetchrow('SELECT 1 FROM projects WHERE project_id = $1', project_id)
     return result is not None
 
-async def add_member(user_connection:Connection, project_id: str, user_id: int, role: str):
-    await user_connection.execute('INSERT INTO project_members (project_id, user_id, role) VALUES ($1, $2, $3)', project_id, user_id, role)
+async def add_member(user_connection:Connection, project_id: str, user_id: int, permission: Json):
+    await user_connection.execute('INSERT INTO project_members (project_id, user_id, permission) VALUES ($1, $2, $3)', project_id, user_id, json.dumps(permission))
 
 async def remove_member(user_connection:Connection, project_id: str, user_id: int):
     await user_connection.execute('DELETE FROM project_members WHERE project_id = $1 AND user_id = $2', project_id, user_id)
 
 async def list_project_members(user_connection:Connection, project_id: str):
-    results = await user_connection.fetch('SELECT user_id, role FROM project_members WHERE project_id = $1', project_id)
-    return [{'user_id': record['user_id'], 'role': record['role']} for record in results]
+    results = await user_connection.fetch('SELECT user_id, permission FROM project_members WHERE project_id = $1', project_id)
+    return [{'user_id': record['user_id'], 'permission': record['permission']} for record in results]
 
-async def change_member_role(user_connection:Connection, project_id: str, user_id: int, new_role: str):
-    await user_connection.execute('UPDATE project_members SET role = $1 WHERE project_id = $2 AND user_id = $3', new_role, project_id, user_id)
+async def change_member_permission(user_connection:Connection, project_id: str, user_id: int, new_permission: Json):
+    await user_connection.execute('UPDATE project_members SET permission = $1 WHERE project_id = $2 AND user_id = $3', json.dumps(new_permission), project_id, user_id)
 
 async def list_user_projects(user_connection:Connection, user_id: int):
-    results = await user_connection.fetch('SELECT project_id, project_name, role FROM projects JOIN project_members USING (project_id) WHERE user_id = $1', user_id)
-    return [{'project_id': record['project_id'], 'project_name': record['project_name'], 'role': record['role']} for record in results]
+    results = await user_connection.fetch('SELECT project_id, project_name, permission FROM projects JOIN project_members USING (project_id) WHERE user_id = $1', user_id)
+    return [{'project_id': record['project_id'], 'project_name': record['project_name'], 'permission': record['permission']} for record in results]
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,8 +1,11 @@
+import json
+
 import pytest
 
 from conftest import user_db_transaction
 from backend.user_management.team_handler import create_team
-from backend.data_management.project_handler import create_project, get_project_name
+from backend.data_management.project_handler import create_project, get_project_name, project_exists, add_member, \
+    list_project_members, change_member_permission, remove_member, delete_project, list_user_projects
 from backend.user_management.user_handler import create_user
 
 
@@ -23,7 +26,34 @@ async def test_setup_project(user_db_transaction, data_db_transaction):
 
     team_id = await create_team(user_connection=user_db_transaction, team_name="Project Team")
 
-    project_id = await create_project(user_connection=user_db_transaction, data_connection=data_db_transaction, project_name=project_name, owner_id=user_id, team_id=team_id)
+    project_id = await create_project(user_connection=user_db_transaction, data_connection=data_db_transaction, project_name=project_name, owner_id=user_id)
     print(f"Created project with ID: {project_id}")
     db_project_name = await get_project_name(user_connection=user_db_transaction, project_id=project_id)
     assert db_project_name == project_name
+    assert project_exists(user_connection=user_db_transaction, project_id=project_id)
+
+    user2_id = await create_user(
+        user_connection=user_db_transaction,
+        userName="project_member",
+        email="other_user@mail.com",
+        password="anotherpassword",
+        lastName="Member",
+        firstName="Project"
+    )
+
+    await add_member(user_connection=user_db_transaction, project_id=project_id, user_id=user2_id, permission={"temp": "member"})
+    members = await list_project_members(user_connection=user_db_transaction, project_id=project_id)
+    print(members)
+    assert any(member['user_id'] == user2_id and json.loads(member['permission']) == {"temp": "member"} for member in members)
+    await change_member_permission(user_connection=user_db_transaction, project_id=project_id, user_id=user2_id, new_permission={"temp": "moderator"})
+    members = await list_project_members(user_connection=user_db_transaction, project_id=project_id)
+    assert any(member['user_id'] == user2_id and json.loads(member['permission']) == {"temp": "moderator"} for member in members)
+
+    await remove_member(user_connection=user_db_transaction, project_id=project_id, user_id=user2_id)
+    members = await list_project_members(user_connection=user_db_transaction, project_id=project_id)
+    assert all(member['user_id'] != user2_id for member in members)
+    projects = await list_user_projects(user_connection=user_db_transaction, user_id=user2_id)
+    assert all(project['project_id'] != project_id for project in projects)
+
+    await delete_project(user_connection=user_db_transaction, data_connection=data_db_transaction, project_id=project_id)
+    assert not await project_exists(user_connection=user_db_transaction, project_id=project_id)
